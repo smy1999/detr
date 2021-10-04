0df65c8104e3350ac6286fac112e59206ac5dc17
smy1999 <865988027@qq.com>
2021-10-04 10:29:46 +0800

    3090 use

--- main.py
+++ main.py
@@ -198,8 +198,7 @@ def main(args):
         if args.distributed:
             sampler_train.set_epoch(epoch)
         train_stats = train_one_epoch(
-            model, criterion, data_loader_train, optimizer, device, epoch,
-            args.clip_max_norm)
+            model, criterion, data_loader_train, optimizer, device, epoch, args.clip_max_norm)
         lr_scheduler.step()
         if args.output_dir:
             checkpoint_paths = [output_dir / 'checkpoint.pth']
--- models/segmentation.py
+++ models/segmentation.py
@@ -63,12 +63,13 @@ class DETRsegm(nn.Module):
 def _expand(tensor, length: int):
     return tensor.unsqueeze(1).repeat(1, int(length), 1, 1, 1).flatten(0, 1)
 
-
+"""
+# 原
 class MaskHeadSmallConv(nn.Module):
-    """
-    Simple convolutional head, using group norm.
-    Upsampling is done using a FPN approach
-    """
+    # 此处有三引号
+    # Simple convolutional head, using group norm.
+    # Upsampling is done using a FPN approach
+    
 
     def __init__(self, dim, fpn_dims, context_dim):
         super().__init__()
@@ -101,7 +102,6 @@ class MaskHeadSmallConv(nn.Module):
 
         x = torch.cat([_expand(x, bbox_mask.shape[1]), bbox_mask.flatten(0, 1)], 1)
 
-        print('1x.shape = ' + str(x.shape))
         x = self.lay1(x)
         x = self.gn1(x)
         x = F.relu(x)
@@ -109,13 +109,10 @@ class MaskHeadSmallConv(nn.Module):
         x = self.gn2(x)
         x = F.relu(x)
         cur_fpn = self.adapter1(fpns[0])
-        print('8x.shape = ' + str(cur_fpn.shape))
 
         if cur_fpn.size(0) != x.size(0):
             cur_fpn = _expand(cur_fpn, x.size(0) // cur_fpn.size(0))
-            print(cur_fpn.size())
-        # x = cur_fpn + F.interpolate(x, size=cur_fpn.shape[-2:], mode="nearest")
-        x = F.interpolate(x, size=cur_fpn.shape[-2:], mode="nearest")
+        x = cur_fpn + F.interpolate(x, size=cur_fpn.shape[-2:], mode="nearest")
         x = self.lay3(x)
         x = self.gn3(x)
         x = F.relu(x)
@@ -131,10 +128,89 @@ class MaskHeadSmallConv(nn.Module):
         cur_fpn = self.adapter3(fpns[2])
         if cur_fpn.size(0) != x.size(0):
             cur_fpn = _expand(cur_fpn, x.size(0) // cur_fpn.size(0))
-        x = cur_fpn + F.interpolate(x, size=cur_fpn.shape[-2:], mode="nearest")
+        x = F.interpolate(x, size=cur_fpn.shape[-2:], mode="nearest")
+        x += cur_fpn
+        # x = cur_fpn + F.interpolate(x, size=cur_fpn.shape[-2:], mode="nearest")
         x = self.lay5(x)
         x = self.gn5(x)
         x = F.relu(x)
+
+        x = self.out_lay(x)
+        print(x.shape)
+        return x
+"""
+
+
+class MaskHeadSmallConv(nn.Module):
+    """
+    Simple convolutional head, using group norm.
+    Upsampling is done using a FPN approach
+    """
+
+    def __init__(self, dim, fpn_dims, context_dim):
+        super().__init__()
+
+        inter_dims = [dim, context_dim // 2, context_dim // 4, context_dim // 8, context_dim // 16, context_dim // 64]
+        self.lay1 = torch.nn.Conv2d(dim, dim, 3, padding=1)
+        self.gn1 = torch.nn.GroupNorm(8, dim)
+        self.lay2 = torch.nn.Conv2d(dim, inter_dims[1], 3, padding=1)
+        self.gn2 = torch.nn.GroupNorm(8, inter_dims[1])
+        self.lay3 = torch.nn.Conv2d(inter_dims[1], inter_dims[2], 3, padding=1)
+        self.gn3 = torch.nn.GroupNorm(8, inter_dims[2])
+        self.lay4 = torch.nn.Conv2d(inter_dims[2], inter_dims[3], 3, padding=1)
+        self.gn4 = torch.nn.GroupNorm(8, inter_dims[3])
+        # self.lay5 = torch.nn.Conv2d(inter_dims[3], inter_dims[4], 3, padding=1)
+        # self.gn5 = torch.nn.GroupNorm(8, inter_dims[4])
+        # self.out_lay = torch.nn.Conv2d(inter_dims[4], 1, 3, padding=1)
+        self.out_lay = torch.nn.Conv2d(inter_dims[3], 1, 3, padding=1)
+
+        self.dim = dim
+
+        self.adapter1 = torch.nn.Conv2d(fpn_dims[0], inter_dims[1], 1)
+        self.adapter2 = torch.nn.Conv2d(fpn_dims[1], inter_dims[2], 1)
+        # self.adapter3 = torch.nn.Conv2d(fpn_dims[2], inter_dims[3], 1)
+
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                nn.init.kaiming_uniform_(m.weight, a=1)
+                nn.init.constant_(m.bias, 0)
+
+    def forward(self, x: Tensor, bbox_mask: Tensor, fpns: List[Tensor]):
+
+        x = torch.cat([_expand(x, bbox_mask.shape[1]), bbox_mask.flatten(0, 1)], 1)
+
+        x = self.lay1(x)
+        x = self.gn1(x)
+        x = F.relu(x)
+        x = self.lay2(x)
+        x = self.gn2(x)
+        x = F.relu(x)
+        cur_fpn = self.adapter1(fpns[0])
+
+        if cur_fpn.size(0) != x.size(0):
+            cur_fpn = _expand(cur_fpn, x.size(0) // cur_fpn.size(0))
+        x = cur_fpn + F.interpolate(x, size=cur_fpn.shape[-2:], mode="nearest")
+        x = self.lay3(x)
+        x = self.gn3(x)
+        x = F.relu(x)
+
+        cur_fpn = self.adapter2(fpns[1])
+        if cur_fpn.size(0) != x.size(0):
+            cur_fpn = _expand(cur_fpn, x.size(0) // cur_fpn.size(0))
+        x = cur_fpn + F.interpolate(x, size=cur_fpn.shape[-2:], mode="nearest")
+        x = self.lay4(x)
+        x = self.gn4(x)
+        x = F.relu(x)
+
+        # cur_fpn = self.adapter3(fpns[2])
+        # if cur_fpn.size(0) != x.size(0):
+        #     cur_fpn = _expand(cur_fpn, x.size(0) // cur_fpn.size(0))
+        # x = F.interpolate(x, size=cur_fpn.shape[-2:], mode="nearest")
+        # x += cur_fpn
+        # # x = cur_fpn + F.interpolate(x, size=cur_fpn.shape[-2:], mode="nearest")
+        # x = self.lay5(x)
+        # x = self.gn5(x)
+        # x = F.relu(x)
 
         x = self.out_lay(x)
         return x
@@ -212,8 +288,14 @@ def sigmoid_focal_loss(inputs, targets, num_boxes, alpha: float = 0.25, gamma: f
     loss = ce_loss * ((1 - p_t) ** gamma)
 
     if alpha >= 0:
-        alpha_t = alpha * targets + (1 - alpha) * (1 - targets)
-        loss = alpha_t * loss
+        # alpha_t = alpha * targets + (1 - alpha) * (1 - targets)
+        alpha1 = alpha * targets
+        alpha2 = (1 - alpha) * (1 - targets)
+        alpha_t = alpha1 + alpha2
+        print(loss.shape)
+        print(targets.shape)
+        # loss = alpha_t * loss
+        loss *= alpha_t
 
     return loss.mean(1).sum() / num_boxes
 
